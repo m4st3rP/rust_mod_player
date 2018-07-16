1d68f9ba3025ab3d6dc5a6ba364b145d07add7c9
Philipp Schwarz <schwarz_philipp@hotmail.de>
2018-07-16 20:28:24 +0200

    added structs for song, pattern and sample

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,31 @@ use std::io::Read;
 
 const TITLE_LENGTH: usize = 20;
 
+struct Sample {
+    name: String,
+    length: u16,
+    finetune: i8,
+    volume: u8,
+    repeat_point: u16,
+    repeat_length: u16
+}
+
+struct Pattern {
+    data: [u8; 1024]
+}
+
+struct Song {
+    title: String,
+    length: u8,
+    special_byte: u8,
+    pattern_positions: [u8; 128],
+    label: String,
+    samples: [Sample; 31],
+    patterns: [Pattern; 127]
+}
+
 fn main() {
-    let file = File::open("addicti.mod").expect("Could not find file."); // TODO temporary use only this file
+    let file = File::open("test.mod").expect("Could not find file."); // TODO temporary use only this file
     let mut buf_reader = BufReader::new(file); // TODO is a buffered reader even useful in this situation?
     let mut file_vector: Vec<u8> = Vec::new();
 
@@ -22,5 +45,19 @@ fn main() {
         title.push(*file_vector_iter.next().unwrap() as char)
     }
 
+
     println!("{}", title);
 }
+/*
+fn get_sample() -> Sample {
+
+}
+
+fn get_pattern() -> Pattern {
+
+}
+
+fn get_song() -> Song {
+
+}
+*/
