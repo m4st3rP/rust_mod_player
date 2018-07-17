892a150cf6cc1b5005afea1be14d59da11ccd9d0
Philipp Schwarz <schwarz_philipp@hotmail.de>
2018-07-17 16:13:52 +0200

    removed crate

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,12 @@
 #![allow(unused_variables)]
 
 extern crate rust_mod;
-extern crate cpal;
 
 use std::fs::File;
 
 use rust_mod::song::*;
-use cpal::EventLoop;
 
 fn main() {
     let file = File::open("test.mod").expect("Could not find file"); // TODO temporary use only this file
     let song = Song::new(file);
-
-    let event_loop = EventLoop::new();
-    let device = cpal::default_output_device().expect("No output device available");
-
-    /*
-    let note_number = 0;
-    let note = Note::new(song.patterns[note_number].data[0], song.patterns[note_number].data[1], song.patterns[note_number].data[2], song.patterns[note_number].data[3]);
-    println!("{}", note.to_string());
-    */
 }
